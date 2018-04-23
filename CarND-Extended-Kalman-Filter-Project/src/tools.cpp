7a18a35844bd16f2d752f48a28b16c2786147928
mkls2319 <mkls2319@yahoo.com>
2018-04-22 21:55:05 -0600

    update

--- CarND-Extended-Kalman-Filter-Project/src/tools.cpp
+++ CarND-Extended-Kalman-Filter-Project/src/tools.cpp
@@ -21,7 +21,7 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   if( (estimations.size() != 0) && (estimations.size() == ground_truth.size()))
   {
 
-    for(int i=0; i < estimations.size(); ++i)
+    for(unsigned int i=0; i < estimations.size(); ++i)
     {
         VectorXd residual = estimations[i] - ground_truth[i];
 
@@ -38,7 +38,7 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   {
     return rmse;
   }
-
+}
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
   /**
