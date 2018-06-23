59210d67ac989821f7826eb0b8e2c68704db68e8
mkls2319 <mkls2319@yahoo.com>
2018-06-23 13:24:28 -0600

    initial

--- CarND-PID-Control-Project/src/PID.cpp
+++ CarND-PID-Control-Project/src/PID.cpp
@@ -11,11 +11,26 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+  this->Kp = Kp;
+  this->Ki = Ki;
+  this->Kd = Kd;
+
+  p_error = numeric_limits<double>::max();
+
+  i_error = 0.0;
+
+  d_error = 0.0;
 }
 
 void PID::UpdateError(double cte) {
+  if (p_error == numeric_limits<double>::max())
+    p_error = cte;
+
+  d_error = cte - p_error;
+  p_error = cte;
+  i_error += cte;
 }
 
 double PID::TotalError() {
+  return Kp * p_error + Kd * d_error + Ki * i_error;
 }
-
--- CarND-PID-Control-Project/src/main.cpp
+++ CarND-PID-Control-Project/src/main.cpp
@@ -34,6 +34,10 @@ int main()
 
   PID pid;
   // TODO: Initialize the pid variable.
+  double kp = 0.2;
+  double ki = 0.0001; // No need to compensate for sytematic bias (we don't have them).
+  double kd = 3.0;
+  pid.Init(kp, ki, kd);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -57,7 +61,7 @@ int main()
           * NOTE: Feel free to play around with the throttle and speed. Maybe use
           * another PID controller to control the speed!
           */
-          
+
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
 
